724c6b4fbb0844931532ebe73ddecfeaeba231f5
sman40901 <sujanmanpr@gmail.com>
2021-02-26 21:23:51 -0800

    first push

---
+++ MOTUGraphQL/Data/Faction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MOTUGraphQL.Data
+{
+   
+    [Table("factionMain")]
+    public class Faction
+    {
+        [Key]
+        [Column("factionId")]
+        public int FactionID { get; set; }
+
+        [Column("baseFaction")]
+        public int BaseFaction { get; set; }
+
+        [Column("factionName")]
+        public string FactionName { get; set; }
+
+        [Column("updated")]
+        public DateTime Updated { get; set; }
+
+        [Column("inserted")]
+        public DateTime Inserted { get; set; }
+    }
+}
---
+++ MOTUGraphQL/Data/MotuContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MOTUGraphQL.Data
+{
+    public class MotuContext : DbContext
+    {
+        public MotuContext(DbContextOptions<MotuContext> options)
+             : base(options)
+        {
+        }
+
+        public DbSet<Faction> Factions { get; set; }
+    }
+}
